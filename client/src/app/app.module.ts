7e7ab634d20ad5e5e402db66cb8e8202f7a3c43d
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-09-26 16:45:10 -0400

    register page functionality

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule }  from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+
+import { UserService } from './providers/user.service';
 
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
@@ -17,13 +23,16 @@ const appRoutes: Routes = [
     AppComponent,
     HomeComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from './../providers/user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  pageTitle = 'Register';
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPass: string = '';
+
+  error: boolean = false;
+  errMsg: string = '';
+  
+  // create instance of UserService
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() { }
+
+  onSubmit(): void {
+    if (this.username == '') {
+      this.errMsg = 'User name is required.';
+      this.error = true;
+    } else if (this.email == '') {
+      this.errMsg = 'Email Address is required.';
+      this.error = true;
+    } else if (this.password == '') {
+      this.errMsg = 'Password is required.';
+      this.error = true;
+    } else if (this.password.length < 8) {
+      this.errMsg = 'Password must be at least 8 chars.';
+      this.error = true;
+    } else if (this.confirmPass == '') {
+      this.errMsg = 'Please confirm password.';
+      this.error = true;
+    } else if (this.password != this.confirmPass) {
+      this.errMsg = 'Passwords do not match';
+      this.error = true;
+    } else {
+      this.error = false;
+      this.errMsg = '';
+
+      // Call UserService to Register
+      this.userService.register(this.username, this.email, this.password).subscribe(data => {
+        if (data['error']) {
+          this.errMsg = 'Registration unsuccessful.';
+          this.error = true;
+        } else {
+          this.router.navigate(['/']); // change to login
+        }
+      });
+    }
+  }
+
+  onReset(): void {
+    this.username = '';
+    this.email = '';
+    this.password = '';
+    this.confirmPass = '';
+
+    this.error = false;
+    this.errMsg = '';
+  }
+
+}
